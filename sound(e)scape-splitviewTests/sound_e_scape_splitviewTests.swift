0b1bffeec1bd3b80205369db531b5e5a17212f83
Obed Cheung <obedcheung@Obeds-MacBook-Air.local>
2017-11-26 18:57:31 +0800

    user interface half laid out

---
+++ sound(e)scape-splitviewTests/sound_e_scape_splitviewTests.swift
@@ -0,0 +1,36 @@
+//
+//  sound_e_scape_splitviewTests.swift
+//  sound(e)scape-splitviewTests
+//
+//  Created by Obed Cheung on 26/11/2017.
+//  Copyright © 2017 obedcheung. All rights reserved.
+//
+
+import XCTest
+@testable import sound_e_scape_splitview
+
+class sound_e_scape_splitviewTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
