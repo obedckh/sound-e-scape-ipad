fa010f65498b026f52aa86a455f7f1b55367d585
Obed Cheung <obedcheung@Obeds-MacBook-Air.local>
2017-11-26 19:30:29 +0800

    user interface 90% laid out .swift created for all controller

---
+++ sound(e)scape-splitview/NotationViewController.swift
@@ -0,0 +1,39 @@
+//
+//  NotationViewController.swift
+//  sound(e)scape-splitview
+//
+//  Created by Obed Cheung on 26/11/2017.
+//  Copyright © 2017 obedcheung. All rights reserved.
+//
+
+import UIKit
+
+class NotationViewController: UIViewController {
+
+    @IBOutlet weak var NotationContainer: UIView!
+    @IBOutlet weak var NotationCategoryTable: UITableView!
+    @IBOutlet weak var NotationExplainScroll: UIScrollView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
